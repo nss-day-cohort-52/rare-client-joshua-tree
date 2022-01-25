0c085f26eddf3adcf7b4254ed7c5ec40c8eb16d5
Kevin Lockett <chlockett@msn.com>
2022-01-25 15:11:45 -0600

    kll-preparing to pull new main

---
+++ src/components/posts/PostDetails.js
@@ -0,0 +1,10 @@
+import React, { useRef } from "react"
+import { Link } from "react-router-dom"
+import { useHistory } from "react-router-dom"
+import { registerUser } from "./AuthManager"
+
+export default () => (
+
+    <section className=""
+
+)
