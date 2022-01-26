2aeb0020634cbce861d57f4df12cf1848fae06c8
Kevin Lockett <chlockett@msn.com>
2022-01-26 11:22:14 -0600

    kll-completed View Post Details

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -42,7 +42,7 @@ export const NavBar = ({ token, setToken }) => {
 				<div className='navbar-start'>
 					{token ? (
 						<Link to='/Posts' className='navbar-item'>
-							Posts
+							All Posts
 						</Link>
 					) : (
 						""
