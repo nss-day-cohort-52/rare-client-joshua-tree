06bc8a0185f0f21a94cc856eb9c470d2a0779b00
charleslong13 <charleslong13@icloud.com>
2022-02-18 11:22:15 -0600

    csl-error

--- src/components/posts/CreateNewPost.js
+++ src/components/posts/CreateNewPost.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { useHistory } from "react-router-dom";
-import { createPost, getPosts } from "./PostManager";
+import { createPost, getPosts, getPostById } from "./PostManager";
 
 
 
